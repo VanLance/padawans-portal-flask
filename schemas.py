67c62a35c508f77f05d87cf9a16830c3c7b27c37
unknown <dylan23smith1990@gmail.com>
2024-01-30 17:16:06 -0600

    add nested followed in user resp

--- schemas.py
+++ schemas.py
@@ -22,3 +22,5 @@ class PostSchemaNested(PostSchema):
 
 class UserSchemaNested(UserSchema):
   posts = fields.List(fields.Nested(PostSchema), dump_only=True)
+  # followed = fields.List(fields.Nested(UserSchema))
+  followed = fields.Function(lambda user: {followed.id: UserSchema().dump(followed) for followed in user.followed} )
